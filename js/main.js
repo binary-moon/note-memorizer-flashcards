46fff74c921ad3baf4221aa8b7154ea51cbfae33
Emre Dokur <emre.dokur@gmail.com.au>
2017-01-02 11:21:59 +1100

    First Build
    First Build

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var flashcards = (function() {
+
+    var notes = ['A', 'A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab'];
+    var strings = ['E6', 'A', 'D', 'G', 'B', 'E1'];
+
+    var $app = $('#app');
+    var $newButton = $app.find('.btn-new');
+    var $stringHolder = $app.find('.card-string .holder');
+    var $noteHolder = $app.find('.card-note .holder');
+
+    var publicApi = {
+        init: function() {
+            flashcards.handleEvents();
+        },
+        handleEvents: function() {
+            $newButton.on('click', function() {
+                flashcards.findRandomString();
+                flashcards.findRandomNote();
+            });
+        },
+        findRandomString: function() {
+            var string = strings[Math.floor(Math.random() * strings.length)];
+            flashcards.displayString(string);
+        },
+        findRandomNote: function() {
+            var note = notes[Math.floor(Math.random() * notes.length)];
+            flashcards.displayNote(note);
+        },
+        displayString: function(string) {
+            $stringHolder.html(string);
+        },
+        displayNote: function(note) {
+            $noteHolder.html(note);
+        }
+    }
+
+    return publicApi;
+})();
+
+$(document).ready(function() {
+    flashcards.init();
+});
